350827a0872c62901114a79b753236d32d0c8c93
unknown <kamillasr15@gmail.com>
2022-10-05 22:41:10 -0300

    Revisão de laços

---
+++ exerciciosloops06.java
@@ -0,0 +1,35 @@
+package exerciciosloops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
+*/
+
+
+
+
+public class exerciciosloops06 {
+
+	public static void main(String[] args) {
+		
+		int fatorial;
+		int multiplicacao=1;
+		
+		Scanner ler=new Scanner(System.in);
+		
+		System.out.println("fatorial:");
+		fatorial=ler.nextInt();
+		
+		System.out.println(fatorial+"!=");
+		
+		for(int i=fatorial;i>=1;i--) {
+			multiplicacao=multiplicacao*i;
+		}
+		
+		
+	System.out.println(multiplicacao);
+	}
+
+}
